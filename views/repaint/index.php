6cd1dc00127a990594b1ce698e7ee2ebda0ac5b7
petanisocial7 <fadiljunaidisiahaan@gmail.com.com>
2024-01-03 18:44:51 +0700

    comment commit

--- views/repaint/index.php
+++ views/repaint/index.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="repaint-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->title)?></h1>
 
     <p>
         <?= Html::a('Create Repaint', ['create'], ['class' => 'btn btn-success']) ?>
